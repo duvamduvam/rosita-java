838cea23a6cdeb4a774071116f4d5c4ff0961ff5
pi <david@mail.duvam.net>
2020-12-30 09:41:47 +0100

    change

--- scripts/rosita.sh
+++ scripts/rosita.sh
@@ -1,7 +1,7 @@
 #/bin/bash
 
 cd /home/pi/rosita
-python3 /home/pi/rosita/rosita-java/py/serialCom.py > /home/pi/rosita/ardiunoOut &
+python3 /home/pi/rosita/rosita-java/py/serialCom.py > /home/pi/rosita/arduinoOut &
 
 cd /home/pi/rosita/rosita-java
 java -jar ./target/rosita-jar-with-dependencies.jar
