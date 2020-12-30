e3e05223ed2e3c459730e218b91ac7e21cc376d9
pi <david@mail.duvam.net>
2020-12-30 09:17:42 +0100

    add scripts

---
+++ scripts/rosita.sh
@@ -0,0 +1,6 @@
+#/bin/bash
+
+cd /home/pi/rosita
+python3 /home/pi/rosita/rosita-java/py/serialCom.py > /home/pi/rosita/ardiunoOut &
+cd /home/pi/rosita/rosita-java
+java -jar ./target/rosita-jar-with-dependencies.jar
